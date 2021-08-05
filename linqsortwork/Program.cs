b0b0ea8be5b3aeb0f8459fa76f3e7dc0eb8567b8
tgendrich <gendricht@gmail.com>
2021-08-05 13:14:03 -0500

    ended

---
+++ linqsortwork/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace linqsortwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var videoGameNames = new List<string>() { "Duke Nukem forever", "Super Mario 64", "Crash Bandicoot", "Resident Evil" }.Where(x => x.Length > 0).OrderBy(x => x.Length).ToList();
+           
+            foreach(string c in videoGameNames)
+                Console.WriteLine(c);
+        }
+    }
+}
